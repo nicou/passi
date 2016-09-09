5d0c5b76483d18f33790fb5c7128b7cb7f9ffb9c
Mika <mika.ropponen@gmail.com>
2016-09-09 19:31:47 +0300

    database authentication

--- passi/src/main/java/fi/softala/ttl/configuration/LoginSecurityConfig.java
+++ passi/src/main/java/fi/softala/ttl/configuration/LoginSecurityConfig.java
@@ -4,6 +4,8 @@ package fi.softala.ttl.configuration;
  * @author Mika Ropponen
  */
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -18,9 +20,21 @@ import org.springframework.web.filter.CharacterEncodingFilter;
 public class LoginSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
-	public void configureGlobal(AuthenticationManagerBuilder authenticationMgr) throws Exception {
-		authenticationMgr.inMemoryAuthentication().withUser("Donald").password("Trump").authorities("ROLE_USER");
+	DataSource dataSource;
+
+	@Autowired
+	public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+		auth.jdbcAuthentication().dataSource(dataSource)
+				.usersByUsernameQuery("SELECT username, password, enabled FROM users WHERE username=?")
+				.authoritiesByUsernameQuery("SELECT username, role from roles WHERE username=?");
 	}
+
+	/*
+	 * @Autowired public void configureGlobal(AuthenticationManagerBuilder
+	 * authenticationMgr) throws Exception {
+	 * authenticationMgr.inMemoryAuthentication().withUser("Donald").password(
+	 * "Trump").authorities("ROLE_USER"); }
+	 */
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -30,36 +44,22 @@ public class LoginSecurityConfig extends WebSecurityConfigurerAdapter {
 		filter.setEncoding("UTF-8");
 		filter.setForceEncoding(true);
 		http.addFilterBefore(filter, CsrfFilter.class);
-		
-		http
-		.authorizeRequests()
-		.anyRequest().authenticated()
-		.antMatchers("/index").hasRole("ADMIN").anyRequest().permitAll()
-		.and()
-		.formLogin()
-		.loginPage("/login").permitAll()
-		.defaultSuccessUrl("/index")
-		.failureUrl("/login?error")
-		.usernameParameter("username")
-		.passwordParameter("password")
-		.and()
-		.logout()
-		.logoutSuccessUrl("/login?logout")
-		.invalidateHttpSession(true)
-		.deleteCookies("remove");
-		
-		http
-		.sessionManagement()
-		.sessionAuthenticationErrorUrl("/login")
-		.invalidSessionUrl("/login");
-		
-		/* old config
-		http
-		.authorizeRequests().antMatchers("/index").access("hasRole('ROLE_USER')").and().formLogin()
-		.loginPage("/login").defaultSuccessUrl("/index").failureUrl("/login?error")
-		.usernameParameter("username").passwordParameter("password").and().logout()
-		.logoutSuccessUrl("/login?logout");
-		*/
+
+		http.authorizeRequests().anyRequest().authenticated().antMatchers("/index").hasRole("ADMIN").anyRequest()
+				.permitAll().and().formLogin().loginPage("/login").permitAll().defaultSuccessUrl("/index")
+				.failureUrl("/login?error").usernameParameter("username").passwordParameter("password").and().logout()
+				.logoutSuccessUrl("/login?logout").invalidateHttpSession(true).deleteCookies("remove");
+
+		http.sessionManagement().sessionAuthenticationErrorUrl("/login").invalidSessionUrl("/login");
+
+		/*
+		 * old config http .authorizeRequests().antMatchers("/index").access(
+		 * "hasRole('ROLE_USER')").and().formLogin()
+		 * .loginPage("/login").defaultSuccessUrl("/index").failureUrl(
+		 * "/login?error")
+		 * .usernameParameter("username").passwordParameter("password").and().
+		 * logout() .logoutSuccessUrl("/login?logout");
+		 */
 
 	}
 }
