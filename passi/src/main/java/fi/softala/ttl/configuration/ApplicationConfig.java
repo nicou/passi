da9ff107661a5d80636097d819e2cab3daa5ecc7
micaro <mika.ropponen@gmail.com>
2016-09-10 03:14:52 +0300

    database credentials from properties file

--- passi/src/main/java/fi/softala/ttl/configuration/ApplicationConfig.java
+++ passi/src/main/java/fi/softala/ttl/configuration/ApplicationConfig.java
@@ -3,26 +3,29 @@
  */
 package fi.softala.ttl.configuration;
 
+// connection pool
 import org.apache.commons.dbcp.BasicDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
 @EnableWebMvc
 @Configuration
 @ComponentScan({ "fi.softala.ttl.*" })
+@PropertySource("classpath:data.properties")
 @Import(value = { LoginSecurityConfig.class })
 public class ApplicationConfig {
 	
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
-	}
+	@Autowired
+	private Environment env;
 	
 	@Bean
 	public InternalResourceViewResolver viewResolver() {
@@ -36,11 +39,12 @@ public class ApplicationConfig {
 	@Bean(name = "dataSource")
 	public BasicDataSource dataSource() {
 		BasicDataSource ds = new BasicDataSource();
-		ds.setDriverClassName("org.mariadb.jdbc.Driver");
-		ds.setUrl("jdbc:mariadb://localhost/projekti"); // ?autoReconnect=true&useSSL=false
-		ds.setUsername("projekti");
-		ds.setPassword("jyDEcx87s");
+		ds.setDriverClassName(env.getProperty("db.driver"));
+		ds.setUrl(env.getProperty("db.url"));
+		ds.setUsername(env.getProperty("db.username"));
+		ds.setPassword(env.getProperty("db.password"));
 		ds.setDefaultAutoCommit(true);
+		ds.setInitialSize(3);
 		return ds;
 	}
 
