1f647b9a3c8ff0a618218f6fcb9ffe6976a88627
Heinonen Roope <a1400174@myy.haaga-helia.fi>
2016-11-08 15:06:09 +0200

    added functions for imagerotation, fixed/upgraded multiselect validator/checker

--- passi/src/main/webapp/static/script/jquery-index.js
+++ passi/src/main/webapp/static/script/jquery-index.js
@@ -8,10 +8,32 @@ $(document).ready(function() {
 	/* indicate selected ball
 	   send the ball value to right/current answer (select by id) */
 
-/* FIX THIS: Function below affects on all waypoint feedback balls. Should affect only one waypoint at a time. */	
-	
+	//display selected ball in the GUI
+	//removes all selected classes only from the current answer, check which button was clicked and add selected classes
 	$('.custom-ball').on('click', function (){
-		$('.custom-ball').removeClass('button-green-selected button-yellow-selected button-red-selected');
+		let balls = $(this).closest("ul").children();
+		$(balls).removeClass('button-green-selected button-yellow-selected button-red-selected');
+		
+		switch($( this ).attr('class').split(' ')[1]){
+			case 'button-green':
+				$(this).addClass('button-green-selected').addClass('teacher-multichoice-selected');
+				break;
+			case 'button-yellow':
+				$(this).addClass('button-yellow-selected').addClass('teacher-multichoice-selected');
+				break;
+			case 'button-red':
+				$(this).addClass('button-red-selected').addClass('teacher-multichoice-selected');
+				break;
+			}
+		var currentAnswerId = $(this).closest("div").prop("id");
+		var currentBallValue = $(this).val();
+		$('#' + currentAnswerId).val(currentBallValue);
+	});
+	
+	/* WORKING CUSTOMBALL SELECTION DISPLAYER FUNCTION - OLD SOLUTION
+	 $('.custom-ball').on('click', function (){
+		let balls = $(this).closest("ul").children();
+		$(balls).removeClass('button-green-selected button-yellow-selected button-red-selected');
 		if ($(this).hasClass('button-green')) {
 			$(this).addClass('button-green-selected');
 			$(this).addClass('teacher-multichoice-selected');
@@ -26,9 +48,11 @@ $(document).ready(function() {
 		var currentBallValue = $(this).val();
 		$('#' + currentAnswerId).val(currentBallValue);
 	});
+	 */
+	
 
 	// change the text in the submit button when clicking
-	$('.assesment-button').on('click', function (){
+	$('.assessment-button').on('click', function (){
 		var isCollapsed = $(this).hasClass('visible');
 		if(isCollapsed) {
 			$(this).removeClass('visible');
@@ -43,36 +67,44 @@ $(document).ready(function() {
 	$('.palaute').on('click', function (e){
 		e.preventDefault();
 		var id = $(this).val();
-		var textarea = $(".teacher-assesment-text-" + id).val().trim().length > 0;
+		var textarea = $(".teacher-assessment-text-" + id).val().trim().length > 0;
 		var multichoice = $('#multichoices-'+ id).children().is('.teacher-multichoice-selected');
 		if (textarea && multichoice){
-			console.log("joo");
 			$('#saveFeedback-' + id).submit();
+			console.log(id);
 			$('.success-toast-' + id).delay(2000).fadeIn(1000).delay(2000).fadeOut(1000);
 		} else {
-			console.log("ei");
 			$('.error-toast-' + id).fadeIn(1000).delay(3000).fadeOut(1000);
 		}
 	});
 
-	/* rotate image right 90 degrees, image and rotateButton should be inside a container div in order for the selector to work
+	//rotate image right 90 degrees, image and rotateButton should be inside a container div in order for the selector to work
 	var angle = 0;
-	 $('.rotateButton').on('click', function() {
+	var rotateImage = function(target) {
 		angle += 90
-		var elem = $(this).prev();
-			elem.css('transform','rotate(' + angle + 'deg)');
-	});
-	*/
+		$(target).css('transform','rotate(' + angle + 'deg)');
+	};
 	
-	$('.lightbox_trigger').click(function(e) {
+	 $('#lightbox').on('click', function(e) {
+		 let target = $(e.target);
+		 target.is('img') ? rotateImage(target) : $('#lightbox').hide();
+		});
 		
+	 //for Displaying clicked image in lightbox
+	 //get the clicked image, reset angle for image rotation, set image css according window, append image and text, show lightbox
+	$('.lightbox_trigger').on('click', function(e) {
 		e.preventDefault();
-		var image_href = $(this).attr("href");
+		let image_href = $(this).attr("href");
+		let image = '<img src="' + image_href + '"/>';
+			angle = 0;
+				$( this ).css('max-width', '' + (window.innerWidth - 50) + 'px').css('max-height', '' + (window.innerHeigt - 50) + 'px');
+					$('#content').html(image);
+					$('#content').append('<p style="margin-top: 50px; color: white">Klikkaa kääntääksesi kuvaa</p>');
+					$('#lightbox').show();
 		
+		/*	for creating #lightbox dynamically
 		if ($('#lightbox').length > 0) {
-			$('#content').html('<img src="' + image_href + '" />');
-			$('#lightbox').show();
-		} else {
+			} else {
 			var lightbox = 
 			'<div id="lightbox">' +
 				'<p>Click to close</p>' +
@@ -81,11 +113,7 @@ $(document).ready(function() {
 				'</div>' +	
 			'</div>';
 			$('body').append(lightbox);
-		}
+		} 
+		*/
 	});
-	
-	$('#lightbox').on('click', function() {
-		$('#lightbox').hide();
-	});
-	
 });
