07642dcf879d67a5b7704a2dc97331e493cddc59
roopehub <roope.heinonen94@gmail.com>
2016-12-13 02:13:17 +0200

    made changes to the scaleimage function in jquery-index

--- passi/src/main/webapp/static/script/jquery-index.js
+++ passi/src/main/webapp/static/script/jquery-index.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
 	//display selected ball in the GUI
 	//removes all selected classes only from the current answer, check which button was clicked and add selected classes
 	$('.custom-ball').on('click', function (){
-		var balls = $(this).closest("ul").children();
+		let balls = $(this).closest("ul").children();
 		$(balls).removeClass('button-green-selected button-yellow-selected button-red-selected');
 		
 		switch($( this ).attr('class').split(' ')[1]){
@@ -77,7 +77,100 @@ $(document).ready(function() {
 		}
 	});
 	
-	var scaleImage = function(rotated) {
+	var scaleImage = function() {
+		var $img = $('#zImage'),
+	    maxWidth = $(window).width(),
+	    maxHeight = $(window).height(),
+	    imageWidth, imageHeight;
+		
+	//toggle width and height properties if image rotated	
+		imageWidth = $img[0].width;
+		imageHeight = $img[0].height;
+		
+		var ratio = [maxWidth / imageWidth, maxHeight / imageHeight ];
+		ratio = Math.min(ratio[0], ratio[1]);
+		
+	//resize the image relative to the ratio
+		$img.attr('width', imageWidth * ratio)
+	    	.attr('height', imageHeight * ratio);
+
+	//align the image vertically and horizontally
+		if(imageRotateCounter % 2 !== 0) {
+			$img.css({
+				maxHeight: maxHeight * .5,
+				maxWidth: maxWidth * .5
+			});
+		}
+		else {
+			$img.css({
+				margin: 'auto',
+				position: 'absolute',
+				maxHeight: maxHeight * .75,
+				maxWidth: maxWidth * .75,
+				top: 0,
+				bottom: 0,
+				left: 0,
+				right: 0
+			});
+		}
+	}
+	
+	var imageRotateCounter = 0;
+	
+	//rotate image right 90 degrees, scale the image accordingly (swap height, and width properties)
+	var angle = 0;
+	var rotateImage = function(target) {
+		angle += 90
+		$(target).css('transform','rotate(' + angle + 'deg)');
+		//let condition = $('#zImage').position().top;
+			imageRotateCounter++;
+			scaleImage();
+	};
+	
+	//click event listener for rotating or hiding the image/lightbox
+	 $('#lightbox').on('click', function(e) {
+		 let target = $(e.target);
+		 target.is('img') ? rotateImage(target) : $('#lightbox').hide();
+		});
+		
+	 //for Displaying clicked image in lightbox
+	 //get the clicked image, reset angle for image rotation, set image css according window, append and scale image, show lightbox
+	$('.lightbox_trigger').on('click', function(e) {
+		e.preventDefault();
+		let image_href = $(this).attr("href");
+		var image = '<img src="' + image_href + '" id="zImage"/>';
+			angle = 0;
+			$('#content').html(image);
+			  $(image).on('load', function(){
+				  scaleImage(false);
+					if($('.glyphicon-repeat').length < 1){
+						$('#content').append('<div id="rotateiconContainer"><p><span class="glyphicon glyphicon-repeat text-center" style="padding-right: 5px;"></span> Klikkaa kuvaa</p></div>');
+					}	
+				});
+			  
+	//Scale image each time the browser window is resized	  
+	$(window).on('resize', function(){
+			scaleImage();
+	});
+			  
+			  /*
+			   * if needed init in lightbox_trigger: //wWidth = $(window).width(), wHeight = $(window).height();
+			 var  wWidth, wHeight, iWidth, iHeigth
+			  if(imageWidth > wWidth || imageHeigth > wHeight){
+				  while(imageWidth > wWidth || imageHeigth > wHeight ) {
+					  	imageWidth *= 0,8;
+					  	imageHeigth *= 0,8;
+					  }
+			  }
+			*/
+		
+			$('#lightbox').show();
+	});
+});
+
+
+/* OLD IMAGE SCALE / BACKUP
+	 * 	var scaleImage = function(rotated) {
 		var $img = $('#zImage'),
 	    maxWidth = $(window).width(),
 	    maxHeight = $(window).height(),
@@ -93,7 +186,7 @@ $(document).ready(function() {
 		}
 
 	var ratio = widthRatio;
-
+	console.log(maxWidth / imageWidth);
 		if (widthRatio * imageHeight > maxHeight) {
 			ratio = heightRatio;
 		}
@@ -111,72 +204,5 @@ $(document).ready(function() {
 			left: 0,
 			right: 0
 		});
-	}
-	
-	var counter = 0;
-	
-	//rotate image right 90 degrees, scale the image accordingly (swap height, and width properties)
-	var angle = 0;
-	var rotateImage = function(target) {
-		angle += 90
-		$(target).css('transform','rotate(' + angle + 'deg)');
-		var condition = $('#zImage').position().top;
-		if(counter % 2 === 0) {
-			counter++;
-			scaleImage(true);
-		} else {
-			counter++;
-			scaleImage(false);
-		}
-	};
-	
-	//click event listener for rotating or hiding the image/lightbox
-	 $('#lightbox').on('click', function(e) {
-		 var target = $(e.target);
-		 target.is('img') ? rotateImage(target) : $('#lightbox').hide();
-		});
-		
-	 //for Displaying clicked image in lightbox
-	 //get the clicked image, reset angle for image rotation, set image css according window, append and scale image, show lightbox
-	$('.lightbox_trigger').on('click', function(e) {
-		e.preventDefault();
-		var image_href = $(this).attr("href");
-		var image = '<img src="' + image_href + '" id="zImage"/>';
-			angle = 0;
-			$('#content').html(image);
-			  $(image).on('load', function(){
-				  scaleImage(false);
-					if($('.glyphicon-repeat').length < 1){
-						$('#content').append('<div id="rotateiconContainer"><p><span class="glyphicon glyphicon-repeat text-center" style="padding-right: 5px;"></span> Klikkaa kuvaa</p></div>');
-					}	
-				});
-			  
-			  /*
-			   * if needed init in lightbox_trigger: //wWidth = $(window).width(), wHeight = $(window).height();
-			 var  wWidth, wHeight, iWidth, iHeigth
-			  if(imageWidth > wWidth || imageHeigth > wHeight){
-				  while(imageWidth > wWidth || imageHeigth > wHeight ) {
-					  	imageWidth *= 0,8;
-					  	imageHeigth *= 0,8;
-					  }
-			  }
-			*/
-		
-		
-			$('#lightbox').show();
-		
-		/*	for creating #lightbox dynamically // NOT IN USE, DELETE?
-		if ($('#lightbox').length > 0) {
-			} else {
-			var lightbox = 
-			'<div id="lightbox">' +
-				'<p>Click to close</p>' +
-				'<div id="content">' + 
-					'<img src="' + image_href +'" />' +
-				'</div>' +	
-			'</div>';
-			$('body').append(lightbox);
-		} 
-		*/
-	});
-});
+	}	
+ */
